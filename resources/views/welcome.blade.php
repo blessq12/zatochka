8c9232bf544fdfcf3168f902fd1efa741026d659
Andrew Master <code70@inbox.ru>
2025-06-12 19:33:30 +0700

    update head

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
-    <meta name="description" content="">
+    <title>Zatochka.tsk - пора точить инструменты</title>
+    <meta name="description" content="Профессиональные услуги по заточке инструмента в Томске">
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
