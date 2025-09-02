5c7918351e3b86ef975b3499302b671f03cf574f
Andrew Master <code70@inbox.ru>
2025-09-03 02:21:13 +0700

    intermediate commit/ generate docs for tech that used in app

---
+++ app/Filament/Resources/Manager/BonusTransactionResource/Pages/ViewBonusTransaction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Manager\BonusTransactionResource\Pages;
+
+use App\Filament\Resources\Manager\BonusTransactionResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewBonusTransaction extends ViewRecord
+{
+    protected static string $resource = BonusTransactionResource::class;
+}
