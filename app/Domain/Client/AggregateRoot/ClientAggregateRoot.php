59ae7597573c3d1cee429f74d457d90820cb6b1c
Andrew Master <code70@inbox.ru>
2025-09-13 20:32:54 +0700

    intermediate

--- app/Domain/Client/AggregateRoot/ClientAggregateRoot.php
+++ app/Domain/Client/AggregateRoot/ClientAggregateRoot.php
@@ -3,6 +3,7 @@
 namespace App\Domain\Client\AggregateRoot;
 
 use App\Domain\Client\Event\ClientCreated;
+use App\Domain\Client\Event\ClientUpdated;
 use Spatie\EventSourcing\AggregateRoots\AggregateRoot;
 
 class ClientAggregateRoot extends AggregateRoot
@@ -10,6 +11,18 @@ class ClientAggregateRoot extends AggregateRoot
     public function createClient(string $clientId, string $phone, string $fullName, array $clientData): self
     {
         $this->recordThat(new ClientCreated(
+            clientId: $clientId,
+            phone: $phone,
+            fullName: $fullName,
+            clientData: $clientData
+        ));
+
+        return $this;
+    }
+
+    public function updateClient(string $clientId, string $phone, string $fullName, array $clientData): self
+    {
+        $this->recordThat(new ClientUpdated(
             clientId: $clientId,
             phone: $phone,
             fullName: $fullName,
