ef8599f8a84dcc6247bfc283c38b3a4a88fce0fe
Andrew Master <code70@inbox.ru>
2026-01-17 23:21:53 +0700

    Enable and configure Laravel Telescope for error monitoring

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,16 +2,9 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\MiddlewareServiceProvider::class,
-    // filament panels providers
     App\Providers\Filament\AdminPanelProvider::class,
-
-    // fortify service provider
     App\Providers\FortifyServiceProvider::class,
     App\Providers\MiddlewareServiceProvider::class,
-    // telescope service provider
-    // App\Providers\TelescopeServiceProvider::class,
-
+    App\Providers\TelescopeServiceProvider::class,
     Laravel\Fortify\FortifyServiceProvider::class,
-
 ];
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 // SPA catch-all маршрут - возвращает layout для всех путей
+// Исключаем Telescope и Filament из catch-all
 Route::get('/{any?}', [MainController::class, 'index'])
-    ->where('any', '.*')
+    ->where('any', '^(?!telescope|cp).*')
     ->name('spa');
